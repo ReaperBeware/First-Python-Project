cd3eff24a06c48c782ff75cc7d8fcc4c7dd9680a
ReaperBeware <Nikhils1220@gmail.com>
2024-01-03 21:18:14 -0700

    Third Boss Killed

--- Elemental Warfare RPG.py
+++ Elemental Warfare RPG.py
@@ -137,6 +137,7 @@ elif element == "Lightning":
 print("You are now ready to begin your adventure! Type anything to continue!")
 input("> ")
 
+# main game
 player = Character(first, last, nickname, element, 1, light, None, None)
 player.apply_buff()
 enemy_1 = Character("Bol", "Dar", "The Mushroom", "Fire", 1, "Fire Ball", None, None)
@@ -181,7 +182,7 @@ print("Congratulations on defeating your first opponent!")
 print("You are officially level {}! Now it is time to battle the second enemy! If you defeat this enemy you will receive 3 levels and unlock your strong attack!".format(player.level))
 print("Your second enemy is {first} {last}, also known as {nick}. His level is {level} so this battle is a bit more difficult! He uses the {element} element and uses the {attack} attack.".format(first=enemy_2.first, last=enemy_2.last, nick=enemy_2.nickname, level=enemy_2.level, element=enemy_2.element, attack=enemy_2.light))
 print("This enemy attacks back so be careful! Enter anything to continue.")
-f = input("> ")
+input("> ")
 
 print("Enter the name of your attack, {}, to attack your opponent.".format(player.light))
 
@@ -189,22 +190,102 @@ while True:
     attack_2 = input("> ")
 
     while attack_2 != player.light:
-        print("Make sure to enter the name of your attack, {}, to atack your opponent.".format(player.light))
+        print("Make sure to enter the name of your attack, {}, to attack your opponent.".format(player.light))
         attack_2 = input("> ")
 
     if attack_2 == player.light:
         enemy_2.health -= player.light_attack
+        player.health -= enemy_2.light_attack
 
     if enemy_2.health <= 20 and enemy_2.health >= 0.1:
         print("Keep attacking, your enemys health is at {}.".format(enemy_2.health))
+        print("{nick} hit you with the {attack} attack! Your health is now at {health}".format(nick=enemy_2.nickname, attack=enemy_2.light, health=player.health))
 
     if enemy_2.health <= 0:
         player.apply_level(3)
         break
     
+print("Congratulations on defeating the second enemy!")
+print("You are officially level {}! Now it is time to choose your strong attack! Enter anything to continue.".format(player.level))
+input("> ")
+
+print("You will now choose a strong attack. Your options are below.")
+
+if element == "Fire":
+    print("Flaming Uppercut|Flaming Pillar")
+    strong = input("<")
+    while strong != "Flaming Uppercut" and strong != "Flaming Pillar":
+        print("Make sure to enter the name of the attack correctly, your options are below.")
+        print("Flaming Uppercut|Flaming Pillar")
+        strong = input("> ")
+elif element == "Water":
+    print("Hydro Blast|Water Prison")
+    strong = input("< ")
+    while strong != "Hydro Blast" and strong != "Water Prison":
+        print("Make sure to enter the name of the attack correctly, your options are below.")
+        print("Hydro Blast|Water Prison")
+        strong = input("> ")
+elif element == "Earth":
+    print("Earth Crush|Mud Wave")
+    strong = input("> ")
+    while strong != "Earth Crush" and strong != "Mud Wave":
+        print("Make sure to enter the name of the attack correctly, your options are below.")
+        print("Earth Crush|Mud Wave")
+        strong = input("> ")
+elif element == "Air":
+    print("Suffocation|Tornado")
+    strong = input("> ")
+    while strong != "Suffocation" and strong != "Tornado":
+        print("Make sure to enter the name of the attack correctly, your options are below.")
+        print("Suffocation|Tornado")
+        strong = input("> ")
+elif element == "Lightning":
+    print("Lightning Strike|Electric Field")
+    strong = input("> ")
+    while strong != "Lightning Strike" and strong != "Electric Field":
+        print("Make sure to enter the name of the attack correctly, your options are below.")
+        print("Lightning Strike|Electric Field")
+        strong = input("> ")
+
+player.strong = strong
+enemy_3 = Character("Morris", "Straw", "The Straw Man", "Air", 5, "Air Bullets", "Suffocation", None)
+enemy_3.apply_buff()
+
+print("You now have two moves you can use in your battles! The {light} attack and the {strong} attack".format(light=player.light, strong=player.strong))
+print("There is only one more challenger left before you can reach max level. After defeating this enemy you will gain 4 levels and reach max level!")
+print("Enter anything to continue and face your third opponent!")
+input("> ")
+print("Your third opponent is {} {}, also known as {}. His level is {}, so this battle is a little more difficult! He uses the {} element and uses the {} light attack and the {} Strong Attack.".format(enemy_3.first, enemy_3.last, enemy_3.nickname, enemy_3.level, enemy_3.element, enemy_3.light, enemy_3.strong))
+print("Enter anything to continue.")
+input("> ")
+
+print("Enter the name of your attacks, {} and {}, to attack your opponent.".format(player.light, player.strong))
+
+while True:
+    enemy_3_attks = [enemy_3.light_attack, enemy_3.strong_attack]
+    enemy_3_attk = random.choice(enemy_3_attks)
+    attack_3 = input("> ")
+    while attack_3 != player.light and attack_3 != player.strong:
+        print("Make sure to enter the correct name of your attacks, {} and {}, to attack your opponent.".format(player.light, player.strong))
+        attack_3 = input("> ")
     
+    if attack_3 == player.light:
+        enemy_3.health -= player.light_attack
+        player.health -= enemy_3_attk
+    elif attack_3 == player.strong:
+        enemy_3.health -= player.strong_attack
+        player.health -= enemy_3_attk
+    
+    if enemy_3.health <= enemy_3.max_health and enemy_3.health >= 0.000001:
+        print("Keep attacking, your enemy is now at {} health.".format(enemy_3.health))
+        print("{} hit you with an attack, your health is now at {}.".format(enemy_3.nickname, player.health))
 
+    if enemy_3.health <= 0:
+        player.apply_level(4)
+        break
 
+        
+    
 
 
 
